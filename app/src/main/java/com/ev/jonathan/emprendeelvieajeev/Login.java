b176111a202edc11e0d3f7c0e8bdf8f0df0c4e9c
jpaul34 <jpaul.snp@gmail.com>
2018-01-14 15:38:53 -0500

    Enlace de la pantalla de inicio con la de registro mediante el boton registrarse.

--- app/src/main/java/com/ev/jonathan/emprendeelvieajeev/Login.java
+++ app/src/main/java/com/ev/jonathan/emprendeelvieajeev/Login.java
@@ -1,13 +1,28 @@
 package com.ev.jonathan.emprendeelvieajeev;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
 
 public class Login extends AppCompatActivity {
+    TextView tv_registro;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        tv_registro = (TextView) findViewById(R.id.tv_btnRegistrar);
+
+        tv_registro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intentRegistro = new Intent(Login.this, Registro.class);
+                Login.this.startActivity(intentRegistro);
+            }
+        });
+
     }
 }
