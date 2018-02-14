e19167cd585dec358c888f11f38f24130aecafe1
sammy de la cruz <samy_samantha_5@hotmail.com>
2018-02-13 20:54:53 -0500

    Clase Informacion Personal

--- app/src/main/java/com/ev/jonathan/emprendeelvieajeev/InformacionPersonal.java
+++ app/src/main/java/com/ev/jonathan/emprendeelvieajeev/InformacionPersonal.java
@@ -20,10 +20,25 @@ public class InformacionPersonal extends AppCompatActivity {
     private VolleyRP volley;
     //private Location localizacion;
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_informacion_personal);
+        correoUsuario = getIntent().getExtras().getString("correoUsuario");
+
+        tv_informacionPersonal_nombre = (TextView) findViewById(R.id.tv_informacionPersonal_nombre);
+        tv_informacionPersonal_apellido = (TextView) findViewById(R.id.tv_informacionPersonal_apellido);
+        tv_informacionPersonal_correo =  (TextView) findViewById(R.id.tv_informacionPersonal_correo);
+        tv_informacionPersonal_contraseña = (TextView) findViewById(R.id.tv_informacionPersonal_contraseña);
+        tv_informacionPersonal_fecha_nacimiento = (TextView) findViewById(R.id.tv_informacionPersonal_fecha_nacimiento);
+        tv_informacionPersonal_pais =  (TextView) findViewById(R.id.tv_informacionPersonal_pais);
+        tv_informacionPersonal_ciudad =  (TextView) findViewById(R.id.tv_informacionPersonal_ciudad);
+
+        volley = VolleyRP.getInstance(this);
+        mRequest = volley.getRequestQueue();
+        mostrarDatosUsuario(ipUsuario+correoUsuario);
+
+    }
+
     }
 }
