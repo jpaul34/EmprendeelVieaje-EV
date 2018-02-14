77824b1fd24050d0578279efacb964622d28fee2
sammy de la cruz <samy_samantha_5@hotmail.com>
2018-02-13 20:53:09 -0500

    Clase Informacion Personal

--- app/src/main/java/com/ev/jonathan/emprendeelvieajeev/InformacionPersonal.java
+++ app/src/main/java/com/ev/jonathan/emprendeelvieajeev/InformacionPersonal.java
@@ -4,6 +4,22 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
 public class InformacionPersonal extends AppCompatActivity {
+    private static String ipUsuario = "http://emprendeelviaje.cu.ma/archivosphpEV/usuario_GETCORREO.php?correo=";
+    private static String ipUbicacion = "http://emprendeelviaje.cu.ma/archivosphpEV/ubicacion_GETID.php?id_ubicacion=";
+    private String correoUsuario;
+
+    private TextView tv_informacionPersonal_nombre;
+    private TextView tv_informacionPersonal_apellido;
+    private TextView tv_informacionPersonal_correo;
+    private TextView tv_informacionPersonal_contraseña;
+    private TextView tv_informacionPersonal_fecha_nacimiento;
+    private TextView tv_informacionPersonal_pais;
+    private TextView tv_informacionPersonal_ciudad;
+
+    private RequestQueue mRequest;
+    private VolleyRP volley;
+    //private Location localizacion;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
