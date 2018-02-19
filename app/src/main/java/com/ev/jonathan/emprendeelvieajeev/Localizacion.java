25d79fb14cc5e5942abb4cdfc545cfcd166af0a4
jpaul34 <jpaul.snp@gmail.com>
2018-02-18 23:25:50 -0500

    Actualzizacion de la Clase Localizacion (Version Definitiva)

--- app/src/main/java/com/ev/jonathan/emprendeelvieajeev/Localizacion.java
+++ app/src/main/java/com/ev/jonathan/emprendeelvieajeev/Localizacion.java
@@ -3,76 +3,82 @@ package com.ev.jonathan.emprendeelvieajeev;
 
 import android.location.Location;
 import android.location.LocationListener;
-import android.location.LocationProvider;
 import android.os.Bundle;
-import android.util.Log;
 import android.widget.TextView;
 
-public class Localizacion implements LocationListener {
-    Mapa ubicacion;
-    // Ubicacion ubication;
-    TextView mensaje1;
-    TextView mensaje2;
-    String direccion = "";
+public class Localizacion  implements LocationListener {
+    private Mapa mapaActivity;
+    private LugarTuristico lugarActivity;
+    private TextView mensaje1;
+    private Location localizacion;
+    private int llamada=0;
 
-    public Localizacion(TextView mensaje1, TextView mensaje2){
+    public Localizacion(TextView mensaje1, int num){
         this.mensaje1=mensaje1;
-        this.mensaje2=mensaje2;
-    }
-
-    public Localizacion(String Direccion){
-        this.direccion=direccion;
-    }
-
-    public Mapa getMainActivity() {
-        return ubicacion;
-    }
-
-    public void setLocalizacion(Mapa ubicacion) {
-        this.ubicacion = ubicacion;
+        this.llamada=num;
     }
 
 
-    //public void setLocalizacion(Ubicacion ubicacion) { this.ubication = ubicacion;    }
+    public Mapa getMapActivity() {
+        return mapaActivity;
+    }
+
+    public Mapa getLugarActivity() {
+        return mapaActivity;
+    }
+
+
+    public void setMapaActivity(Mapa mapaActivity) {
+        this.mapaActivity = mapaActivity;
+        this.llamada = 1;
+    }
+
+    public void setLugarActivity(LugarTuristico lugarActivity) {
+        this.lugarActivity = lugarActivity;
+        this.llamada = 2;
+    }
 
     @Override
     public void onLocationChanged(Location loc) {
-        // Este metodo se ejecuta cada vez que el GPS recibe nuevas coordenadas
-        // debido a la deteccion de un cambio de ubicacion
+        // Este mŽtodo se ejecuta cada vez que el GPS recibe nuevas coordenadas
+        // debido a la detecci—n de un cambio de ubicacion
         loc.getLatitude();
         loc.getLongitude();
+        localizacion = loc;
 
-        String Text = "Mi ubicacion actual es: " + "\n Lat = "+ loc.getLatitude() + "\n Long = " + loc.getLongitude();
-        mensaje1.setText(Text);
-
-        //Llmanada al metodo para obtener la direccion
-        this.ubicacion.setLocation(loc);
+        if(llamada == 1 ){
+            mapaActivity.setLocation(loc);
+        }
+        if(llamada == 2){
+            lugarActivity.setLocation(loc);
+        }
     }
 
     @Override
     public void onProviderDisabled(String provider) {
-        // Este metodo se ejecuta cuando el GPS es desactivado
+        // Este mŽtodo se ejecuta cuando el GPS es desactivado
         mensaje1.setText("GPS Desactivado");
     }
 
     @Override
     public void onProviderEnabled(String provider) {
-        // Este metodo se ejecuta cuando el GPS es activado
+        // Este mŽtodo se ejecuta cuando el GPS es activado
         mensaje1.setText("GPS Activado");
+
     }
 
     @Override
     public void onStatusChanged(String provider, int status, Bundle extras) {
-        switch (status) {
-            case LocationProvider.AVAILABLE:
-                Log.d("debug", "LocationProvider.AVAILABLE");
-                break;
-            case LocationProvider.OUT_OF_SERVICE:
-                Log.d("debug", "LocationProvider.OUT_OF_SERVICE");
-                break;
-            case LocationProvider.TEMPORARILY_UNAVAILABLE:
-                Log.d("debug", "LocationProvider.TEMPORARILY_UNAVAILABLE");
-                break;
-        }
+        // Este mŽtodo se ejecuta cada vez que se detecta un cambio en el
+        // status del proveedor de localizaci—n (GPS)
+        // Los diferentes Status son:
+        // OUT_OF_SERVICE -> Si el proveedor esta fuera de servicio
+        // TEMPORARILY_UNAVAILABLE -> Temp˜ralmente no disponible pero se
+        // espera que este disponible en breve
+        // AVAILABLE -> Disponible
+    }
+
+    public Location getLocalizacion() {
+        return localizacion;
     }
 }
