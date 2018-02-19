ebc30c5d8158a2f37d4c192c990108a902f539da
sammy de la cruz <samy_samantha_5@hotmail.com>
2018-02-19 10:03:08 -0500

    Actualización de la clase Acerca de  (Versión definitiva)

--- app/src/main/java/com/ev/jonathan/emprendeelvieajeev/AcercaDe.java
+++ app/src/main/java/com/ev/jonathan/emprendeelvieajeev/AcercaDe.java
@@ -1,7 +1,7 @@
 package com.ev.jonathan.emprendeelvieajeev;
 
-import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 
 public class AcercaDe extends AppCompatActivity {
 
