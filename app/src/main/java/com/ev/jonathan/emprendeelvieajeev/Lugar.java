fff59122279960fe31f858a5538634dddced3a98
jpaul34 <jpaul.snp@gmail.com>
2018-02-18 23:24:01 -0500

    Creacion y edicion del objeto Lugar (Utilizada para los datos del lugar)

---
+++ app/src/main/java/com/ev/jonathan/emprendeelvieajeev/Lugar.java
@@ -0,0 +1,51 @@
+package com.ev.jonathan.emprendeelvieajeev;
+
+public class Lugar {
+
+    private String nombre_lugar;
+    private String informacion;
+    private String tipo;
+
+    public Lugar() {
+
+    }
+
+    public Lugar(String nombre_lugar, String tipo, String informacion) {
+        this.nombre_lugar = nombre_lugar;
+        this.informacion = informacion;
+        this.tipo = tipo;
+    }
+
+    public String getNombre_lugar() {
+        return nombre_lugar;
+    }
+
+    public void setNombre_lugar(String nombre_lugar) {
+        this.nombre_lugar = nombre_lugar;
+    }
+
+    public String getInformacion() {
+        return informacion;
+    }
+
+    public void setInformacion(String informacion) {
+        this.informacion = informacion;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    /*****************************************************************/
+    @Override
+    public String toString() {
+        return  "<p align=\"justify\"><br><b>&nbsp;Nombre:</b> " + nombre_lugar +
+                "<br><b>&nbsp;Tipo de Lugar:</b> " + tipo +
+                "<br><b>&nbsp;Informacion:</b> <br>&nbsp;" + informacion +"<br></p>";
+    }
+
+}
